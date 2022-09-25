1253eccb5d3006404c01a573003d2430a3aee8ef
Ranjodh Singh <workwithranjodh@gmail.com>
2022-09-26 01:00:23 +0530

    used props

--- src/Components/Collections/Collection.jsx
+++ src/Components/Collections/Collection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { CollectCard } from '../Collect-Card/CollectCard'
+// import bestof from '.../images/bestof'
 import './collections.css'
 
 export const Collection = () => {
@@ -10,10 +11,10 @@ export const Collection = () => {
             <p className='text'>Explore curated lists of top restaurants, cafes, pubs, and bars in Dehradun, based on trends</p>
         </div>
         <div className="collect-cards">
-        <CollectCard />
-        <CollectCard />
-        <CollectCard />
-        <CollectCard />
+        <CollectCard name={'Great Cafes'} places={'26'} img ={'https://dx6iy6yk2x0g.cloudfront.net/images/945/b1c/945b1cddf650bc612b01a34ea28f617d51b17901-x425.jpg?compressionQuality=80&id=57fe6d27ab9453b8353e6181&op=scale&version=1'} />
+        <CollectCard name={'Best of Your Area'} places={'20'} img= {'https://i0.wp.com/kohleyedme.com/wp-content/uploads/2019/01/Monastery-1.jpg?w=683&ssl=1'}/>
+        <CollectCard name={'Sweet Tooth'} places={'6'} img={'https://inspiredbycharm.com/wp-content/uploads/2017/03/cupcake-sprinkles.jpg'}/>
+        <CollectCard name={'Family Friendly'} places={'17'} img={'https://media-cdn.tripadvisor.com/media/photo-s/0d/8b/82/2c/lunch-at-dishoom.jpg'}/>
         </div>
 
     </div>
